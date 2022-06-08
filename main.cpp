396bf8deae4b9f7128ec6f0cafb5bc541b7c9988
Abdullah Yaser <buudi.dev@gmail.com>
2022-06-09 06:02:40 +0800

    implemented matrix multiplication Y=AX and printed the resultant Matrix Y

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 // Author: Abdullah Yaser Alnadhari
 // follow me on github: https://github.com/buudi 
 #include <iostream>
-using namespace std;
 
 int main()
 {
@@ -23,8 +22,13 @@ int main()
     // Performing Matrix Multiplication Y=AX
     // Formula: Y[i] = (A[i][0]*X[0]) + (A[i][1]*X[1]) + (A[i][2]*X[2])
     // where i is the iteration in the loop, which reflects: rowNumber+1
+    for (int i=0; i<3; i++)
+    {
+        Y[i] = (A[i][0]*X[0]) + (A[i][1]*X[1]) + (A[i][2]*X[2]);
+    }
 
-    
+    // Printing the new Matrix Y:
+    std::cout << "Y = (" << Y[0] << ", " << Y[1] << ", " << Y[2]<< ") Transpose" << std::endl;
 
 }
 
