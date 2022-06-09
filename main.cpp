c68959ba75efa17821b08e8fa9aa6ee7466c3643
Abdullah Yaser <buudi.dev@gmail.com>
2022-06-09 08:28:35 +0800

    made temporary iteration, error condition not added yet

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 // Author: Abdullah Yaser Alnadhari
 // follow me on github: https://github.com/buudi 
+// check latest updates for this program: https://github.com/buudi/Dominant-EigenValue 
 #include <iostream>
+#include <cstdlib>
 
-int* multiply_AX(int A[3][3], int X[3], int Y[3])
+float* multiply_AX(int A[3][3], float X[3], float Y[3])
 {
     // Performing Matrix Multiplication Y=AX
     // Formula: Y[i] = (A[i][0]*X[0]) + (A[i][1]*X[1]) + (A[i][2]*X[2])
-    // where i is the iteration in the loop, which reflects: rowNumber+1
+    // where i is the iteration in the loop, which reflects the row number
 
     for (int i=0; i<3; i++)
     {
@@ -26,25 +28,66 @@ int main()
     };
 
     // defining Matrix X
-    int X[3] = {0, 1, 0};
+    float X[3] = {0, 1, 0};
 
     // New Matrix Y that will be the result of
     // matrix Multiplication Y=AX, 
     // initially it holds (0 0 0) transposed
-    int Y[3]={0, 0, 0};
+    float Y[3]={0, 0, 0};
 
     multiply_AX(A, X, Y);
 
     // Printing the new Matrix Y:
-    std::cout << "Y = (" << Y[0] << ", " << Y[1] << ", " << Y[2]<< ") Transpose" << std::endl;
-
-    // defining the error as delta:
-    float delta = 0.0005;
+    // std::cout << "Y = (" << Y[0] << ", " << Y[1] << ", " << Y[2]<< ") Transpose" << std::endl;
 
     // defining a 3x1 matrix which will hold the
     // dominant eigenvector once the iteration ends
-    float eigenvector[3] = {0, 0, 0};
+    float V[3] = {0, 0, 0};
 
+    // first let the initial EigenVector equal X
+    for (int i=0; i < 3; i++)
+    {
+        V[i] = X[i];
+    }
 
+    // defining the error value as delta:
+    float delta = 0.0005;
+
+    // 2nd we initilize a holder for current error, the A*V vector and 
+    // for the eigen value m:
+    float AV[3] = {0, 0, 0};
+    float m = 0;
+    float tempM[9];
+    float error;
+
+    // now we iterate to find the eigenvector: 
+    for (int i=0; i < 7; i++)
+    {
+        multiply_AX(A, V, AV);
+        for (int i=0; i<3; i++)
+        {
+            if (AV[i] > m)
+                m = AV[i];
+        }
+        tempM[i] = m;
+        std::cout << "m " <<i << ":" << m << std::endl;
+        for (int i=0; i < 3; i++)
+        {
+            V[i] = AV[i] / m;
+        } 
+        if(i != 0)
+        {
+            error = m - tempM[i-1];
+            std::cout << "Error " << i << ":" <<  error << std::endl;
+        } 
+    }
+    
+    // std::cout << "m: " << m << std::endl;
+    std::cout << "Eigenvector: "  << std::endl;
+    std::cout << V[0] << std::endl; 
+    std::cout << V[1] << std::endl; 
+    std::cout << V[2] << std::endl; 
+
+    system("pause");
+    return 0;
 }
-
